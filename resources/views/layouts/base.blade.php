93c92fca48c9d2cc0b61cf6b0f25bd3dfca4cf44
farid fadilah permana <faridfadillah29@gmail.com>
2023-01-17 14:39:43 +0700

    change font to Nunito font

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -15,7 +15,10 @@
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&display=swap" rel="stylesheet">
+    
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     @livewireStyles
     @livewireScripts
@@ -33,6 +36,7 @@
         <!-- Leaflet custom style -->
         <style>
             .leaflet-tooltip {
+                font-family: 'Nunito', sans-serif;
                 background: white;
                 color: black;
                 box-shadow: none;
