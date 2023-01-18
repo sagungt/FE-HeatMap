17769c18aad539f193cd0098f2e5116c17e0d0db
farid fadilah permana <faridfadillah29@gmail.com>
2023-01-18 13:47:25 +0700

    change button mode to components livewire

---
+++ resources/views/livewire/components/mode.blade.php
@@ -0,0 +1,3 @@
+<div class="absolute hidden lg:flex items-center gap-5 top-20 right-5 z-[9999]">
+    <button id='mode' class="text-white transition duration-200 bg-purple-700 hover:bg-purple-700/80 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 ">Hover mode</button>
+</div>
