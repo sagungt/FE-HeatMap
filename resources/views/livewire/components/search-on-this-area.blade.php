3dde90a2cd5c357c42233b7f3b3fe2b178aab2f1
sagungt <agungjordan.aj@gmail.com>
2023-01-29 23:19:41 +0700

    fix styling

--- resources/views/livewire/components/search-on-this-area.blade.php
+++ resources/views/livewire/components/search-on-this-area.blade.php
@@ -1,4 +1,4 @@
-<button id="search-on-this-area" onclick="resetHeatmap()" class="opacity-0 transition-all search-on-this-area cursor-pointer absolute z-[9999999] flex justify-center items-center w-12 h-12 text-slate-700 duration-500 rounded-full bg-white shadow-lg bottom-28 right-5 lg:right-[50%] lg:gap-3 lg:top-5 lg:text-xs lg:w-[170px] lg:h-[40px] lg:rounded-lg lg:translate-x-[50%]">
+<button id="search-on-this-area" onclick="resetHeatmap()" class="opacity-0 transition-all search-on-this-area cursor-pointer absolute z-[9999999] flex justify-center items-center w-12 h-12 text-slate-700 duration-500 rounded-full bg-white shadow-lg bottom-28 right-5 lg:right-[50%] lg:gap-3 lg:top-5 lg:text-xs lg:w-[170px] lg:h-[40px] lg:rounded-lg lg:translate-x-[50%] hover:bg-slate-100 group">
     <i class="fa-solid fa-magnifying-glass-chart text-xl"></i>
     <p class="hidden lg:inline-block">Search on this area</p>
 </button>
